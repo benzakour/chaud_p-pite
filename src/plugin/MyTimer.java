4d6f6a475fc449a55dc333095e6fac90a77d8388
Benzakour Souha <benzakour@a15p22.fil.univ-lille1.fr>
2015-12-01 11:14:33 +0100

    modification MyTimer

--- src/plugin/MyTimer.java
+++ src/plugin/MyTimer.java
@@ -6,26 +6,30 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.Timer;
 
-public class MyTimer {
+public class MyTimer implements ActionListener  {
 
 	protected Timer timer;
+	protected final ActionListener listener;
 	
-	public MyTimer() {
-		this.timer = new Timer(1000, new MyTimerActionListener());
+	public MyTimer(ActionListener listener) {
+		this.listener= listener;
 	}
 
     public void start(int milliseconds){
     	if(timer!=null){
+    		stop();
     	} 
+    	timer= new Timer(milliseconds, this);
+    	timer.start();
     }
 
+    public void stop(){
+    	timer.stop();
+    	timer=null;
+    }
 
+	public void actionPerformed(ActionEvent e) {
+		listener.actionPerformed(e);
+	}
 
-class MyTimerActionListener implements ActionListener {
-  public void actionPerformed(ActionEvent e) {
-
-    System.out.println("asdf");
-
-  }
-}
 }
