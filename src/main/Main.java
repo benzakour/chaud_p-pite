eb65e565bd9e36a61ab528cc78d3d0422d83fbee
Benzakour Souha <benzakour@a15p22.fil.univ-lille1.fr>
2015-12-01 11:40:21 +0100

    modification MyTimer and MyListner

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,15 @@
 package main;
 
-public class Main {
+import java.awt.event.ActionListener;
 
+import plugin.MyListener;
+import plugin.MyTimer;
+
+public class Main {
+	public static void main(String[] args) {
+		ActionListener listener= new MyListener();
+		MyTimer t = new MyTimer(listener);
+		t.start(1000);
+		while (true);
+}
 }
